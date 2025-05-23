905f3701258bb55eb8226333db7dd10ffcdd24aa
Prajodeep13 <prajodeep13@gmail.com>
2025-05-23 17:37:21 +0530

    Updated code

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@ session_start();
   <nav>
     <?php if (isset($_SESSION['user'])): ?>
       <a href="/dashboard.php">Dashboard</a>
-      <a href="/logout.php">Logout</a>
+      <a href="/logout.php">Logout</a> 
     <?php else: ?>
       <a href="/login.php">Login</a>
       <a href="/register.php">Register</a>
